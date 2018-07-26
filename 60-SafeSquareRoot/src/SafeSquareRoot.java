88a0ec968c340a02a96b0a4bf3eeed9af40c156d
solutionman <solutionman@inbox.ru>
2018-07-26 22:32:41 +0700

    '60-SafeSquareRoot'

--- 60-SafeSquareRoot/src/SafeSquareRoot.java
+++ 60-SafeSquareRoot/src/SafeSquareRoot.java
@@ -6,12 +6,23 @@ public class SafeSquareRoot {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        double userInput = scanner.nextDouble();
-        if(userInput < 0){
-            System.out.println("You can't take the square root of a negative number, silly");
-            return;
+        try{
+            double userInput = scanner.nextDouble();
+            if(userInput < 0){
+                System.out.println("You can't take the square root of a negative number, silly");
+                return;
+            }
+            try{
+                double result = Math.sqrt(userInput);
+                System.out.printf("The square root of %f is %f ",userInput, result);
+            } catch(Exception e){
+                System.out.println("Can't get square root of " + userInput);
+            }
+        } catch(Exception e){
+            System.out.println("Can't read this as a number");
         }
-        double result = Math.sqrt(userInput);
-        System.out.printf("The square root of %f is %f ",userInput, result);
+
+
+
     }
 }
